3d4dfe616124312201a8469415f3c389527ac323
Anton Kats <antonkats@gmail.com>
2020-02-01 12:48:31 -0400

    independent movement

--- Assets/char1move.cs
+++ Assets/char1move.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class char1move : MonoBehaviour
 {
-    public float moveSpeed1 = 5f;
+    public float moveSpeed = 5f;
+
+    public int playerNum;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log(playerNum);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement1 = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement1 * Time.deltaTime * moveSpeed1;
+        float dir = 0;
+        if (playerNum == 0) {
+            dir = Input.GetAxis("Horizontal");
+            Debug.Log(dir);
+        } else {
+            // if (Input.GetKeyDown(KeyCode.A)) {
+            //     dir = -1;
+            // } else if (Input.GetKeyDown(KeyCode.D)) {
+            //     dir = 1;
+            // }
+            dir = Input.GetAxis("HorizontalB");
+        }
 
-
+        Vector3 movement = new Vector3(dir, 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
     }
 }
