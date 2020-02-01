88123474b46bed45e632a15ad889b12f9f6f25b9
John Green <38727877+Jaggaroo@users.noreply.github.com>
2020-02-01 08:06:41 -0800

    Initial

---
+++ Assets/char1move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char1move : MonoBehaviour
+{
+    public float moveSpeed1 = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement1 = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement1 * Time.deltaTime * moveSpeed1;
+
+
+    }
+}
---
+++ Assets/char2move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char2move : MonoBehaviour
+{
+    public float moveSpeed2 = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement2 = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement2 * Time.deltaTime * moveSpeed2;
+
+
+    }
+}
