7b0192b1287bca0593884a187165a53abe9303d5
John Green <38727877+Jaggaroo@users.noreply.github.com>
2020-02-01 09:26:10 -0800

    Merged separete movement

--- Assets/char1move.cs
+++ Assets/char1move.cs
@@ -7,11 +7,16 @@ public class char1move : MonoBehaviour
     public float moveSpeed = 5f;
 
     public int playerNum;
+    public float moveSpeed1 = 5f;
+
+    Vector2 screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+    Vector2 screenOrigo = Camera.main.ScreenToWorldPoint(Vector2.zero);
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(playerNum);
+        Debug.Log(screenBounds);
     }
 
     // Update is called once per frame
@@ -30,7 +35,26 @@ public class char1move : MonoBehaviour
             dir = Input.GetAxis("HorizontalB");
         }
 
+        //Vector3 movement1 = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        //transform.position += movement1 * Time.deltaTime * moveSpeed1;
+
+        //else if (pos.x < screenOrigo.x)
+        //{
+        //    transform.position = screenOrigo.x;
+        //}
+        //else if (pos.y > screenBounds.y || pos.y < screenOrigo.y)
+        //{
+        //    DoStuff();
+        //}
+
         Vector3 movement = new Vector3(dir, 0f, 0f);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        //Vector3 pos = transform.position;
+        //if (pos.x > screenBounds.x)
+        //{
+        //    transform.position = new Vector3(screenBounds.x, pos.y, 0);
+        //}
+
     }
 }
