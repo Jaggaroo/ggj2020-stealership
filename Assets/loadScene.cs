764c8a71411d53109801ec77fe840df913dcca28
Anton Kats <antonkats@gmail.com>
2020-02-01 18:45:00 -0400

    fixed bug with spawns not showing up

--- Assets/loadScene.cs
+++ Assets/loadScene.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class loadScene : MonoBehaviour
@@ -7,6 +8,8 @@ public class loadScene : MonoBehaviour
     public float topColliderBottomPos;
 
     private float[] levelsTopPos;
+
+    private GameObject[] objInsts = new GameObject[] {};
 
     // Start is called before the first frame update
     void Start()
@@ -42,23 +45,36 @@ public class loadScene : MonoBehaviour
         float x = Random.Range(minX + prefabWidth, maxX - prefabWidth);
         float y = levelsTopPos[Mathf.RoundToInt(Random.Range(0f, 1f))] - prefabHeight;
 
-        Vector3 pos = new Vector3(x, y, transform.position.z);
+        Vector3 pos = new Vector3(x, y, transform.position.z - 1);
 
         // Debug.Log("HW " + height + " " + width);
         // Debug.Log("X span " + (bottomLeftEdgeVector.x + prefabWidth / 2) + " " + topRightEdgeVector.x);
         // Debug.Log("Random pos init " + x + " " + y);
-        Debug.Log("Random pos toWorld " + pos.x + " " + pos.y + " " + pos.z + " " + prefabHeight + " " + prefabWidth);
+        // Debug.Log("Start pos toWorld " + pos.x + " " + pos.y + " " + pos.z + " " + prefabHeight + " " + prefabWidth);
         // Debug.Log(pos.x);
         // Debug.Log(pos.y);
 
         GameObject inst = Instantiate(prefab, pos, Quaternion.identity);
         Rigidbody2D rb = inst.GetComponent<Rigidbody2D>();
         float tDir = Mathf.Ceil(Random.Range(0, 1)) * 2 - 1;
-        // rb.AddTorque(tDir * Random.Range(150, 200));
+
+        objInsts = objInsts.Concat(new GameObject[] { inst }).ToArray();
+
+        pos = inst.transform.position;
+        // Debug.Log("AfterInst pos toWorld " + pos.x + " " + pos.y + " " + pos.z);
+        rb.AddTorque(tDir * Random.Range(500, 1500));
     }
 
     // Update is called once per frame
     void Update()
     {
+        // float z = transform.position.z;
+        // Debug.Log("BG z " + z);
+        // foreach (GameObject inst in objInsts) {
+        //     Vector3 pos = inst.transform.position;
+        //     inst.transform.position = new Vector3(pos.x, pos.y, -1);
+        //     pos = inst.transform.position;
+        //     Debug.Log(inst + " position is " + pos.x + " " + pos.y + " " + pos.z);
+        // }
     }
 }
