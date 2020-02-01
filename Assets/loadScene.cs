5032b237fa36b02becf2048b433bfe9930d22944
Anton Kats <antonkats@gmail.com>
2020-02-01 14:53:29 -0400

    generating cog

---
+++ Assets/loadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadScene : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab, new Vector3(0,0,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
