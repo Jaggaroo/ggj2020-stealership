bbba48e447985b015bbf28b5e1cfeccfac674cf4
Anton Kats <antonkats@gmail.com>
2020-02-02 13:40:12 -0400

    spawning the right number of objects

--- Assets/ShipPartRegistry.cs
+++ Assets/ShipPartRegistry.cs
@@ -1,14 +1,69 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShipPartRegistry
 {
+    public string[] catShipPartsNames = new string[] {"BrokenWhiteShip", "WhiteRockets"};
+    public string[] eyeShipPartsNames = new string[] {"BrokenBlackShip", "BlackRocket"};
+    public string[] commonPartsNames = new string[] {"Antenna", "Window"};
+    public string[] solePartsNames = new string[] {"Energycore"};
+
     public Object[] catShipParts = new Object[] {};
     public Object[] eyeShipParts = new Object[] {};
     public Object[] commonParts = new Object[] {};
 
+    private float minX;
+    private float maxX;
+    private float[] levelsTopPos;
+
+    public ShipPartRegistry(float _minX, float _maxX, float[] _levelsTopPos) {
+        minX = _minX;
+        maxX = _maxX;
+        levelsTopPos = _levelsTopPos;
+    }
+
     public void SpawnParts() {
-        
+
+        foreach (string name in catShipPartsNames) {
+            SpawnPart((GameObject) Resources.Load("Parts/"+name));
+        }
+
+        foreach (string name in eyeShipPartsNames) {
+            SpawnPart((GameObject) Resources.Load("Parts/"+name));
+        }
+
+        foreach (string name in commonPartsNames) {
+            GameObject obj = (GameObject) Resources.Load("Parts/"+name);
+            SpawnPart(obj);
+            SpawnPart(obj);
+        }
+
+        foreach (string name in solePartsNames) {
+            SpawnPart((GameObject) Resources.Load("Parts/"+name));
+        }
+    }
+
+   void SpawnPart(GameObject prefab)
+    {
+        Vector3 bounds = prefab.GetComponentInChildren<MeshRenderer>().bounds.size;
+
+        float prefabWidth = bounds.x;
+        float prefabHeight = bounds.y;
+
+        float x = Random.Range(minX + prefabWidth, maxX - prefabWidth);
+        float y = levelsTopPos[Mathf.RoundToInt(Random.Range(0f, 1f))] - prefabHeight;
+
+        Vector3 pos = new Vector3(x, y, 0);
+
+        GameObject inst = GameObject.Instantiate(prefab, pos, Quaternion.identity);
+        inst.tag = "part";
+        Rigidbody2D rb = inst.GetComponent<Rigidbody2D>();
+        float tDir = Mathf.Ceil(Random.Range(0, 1)) * 2 - 1;
+
+        // objInsts = objInsts.Concat(new GameObject[] { inst }).ToArray();
+
+        rb.AddTorque(tDir * Random.Range(500, 1500));
     }
 }
+
--- Assets/loadScene.cs
+++ Assets/loadScene.cs
@@ -9,7 +9,7 @@ public class loadScene : MonoBehaviour
 
     private float[] levelsTopPos;
 
-    private GameObject[] objInsts = new GameObject[] {};
+    private ShipPartRegistry registry;
 
     // Start is called before the first frame update
     void Start()
@@ -23,62 +23,13 @@ public class loadScene : MonoBehaviour
         levelsTopPos =
             new float[2] { topRightEdgeVector.y, topColliderBottomPos };
 
-        Object[] prefabs =
-            Resources.LoadAll("Parts");
+        registry = new ShipPartRegistry(bottomLeftEdgeVector.x, topRightEdgeVector.x, levelsTopPos);
 
-        // Debug.Log("Parts count " + prefabs.Length)    ;
-
-        foreach (GameObject _prefab in prefabs) {
-            GameObject prefab = (GameObject) _prefab;
-            
-            // Debug.Log("Prefab " + prefab);
-            SpawnPart(prefab, bottomLeftEdgeVector.x, topRightEdgeVector.x);
-        }
-    }
-
-    void SpawnPart(GameObject prefab, float minX, float maxX)
-    {
-        Vector3 bounds = prefab.GetComponentInChildren<MeshRenderer>().bounds.size;
-
-        float prefabWidth = bounds.x;
-        float prefabHeight = bounds.y;
-
-        // Debug.Log("Prefab size " + prefabWidth + " " + prefabHeight);
-
-        float x = Random.Range(minX + prefabWidth, maxX - prefabWidth);
-        float y = levelsTopPos[Mathf.RoundToInt(Random.Range(0f, 1f))] - prefabHeight;
-
-        Vector3 pos = new Vector3(x, y, transform.position.z - 1);
-
-        // Debug.Log("HW " + height + " " + width);
-        // Debug.Log("X span " + (bottomLeftEdgeVector.x + prefabWidth / 2) + " " + topRightEdgeVector.x);
-        // Debug.Log("Random pos init " + x + " " + y);
-        // Debug.Log("Start pos toWorld " + pos.x + " " + pos.y + " " + pos.z + " " + prefabHeight + " " + prefabWidth);
-        // Debug.Log(pos.x);
-        // Debug.Log(pos.y);
-
-        GameObject inst = Instantiate(prefab, pos, Quaternion.identity);
-        inst.tag = "part";
-        Rigidbody2D rb = inst.GetComponent<Rigidbody2D>();
-        float tDir = Mathf.Ceil(Random.Range(0, 1)) * 2 - 1;
-
-        objInsts = objInsts.Concat(new GameObject[] { inst }).ToArray();
-
-        pos = inst.transform.position;
-        // Debug.Log("AfterInst pos toWorld " + pos.x + " " + pos.y + " " + pos.z);
-        rb.AddTorque(tDir * Random.Range(500, 1500));
+        registry.SpawnParts();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // float z = transform.position.z;
-        // Debug.Log("BG z " + z);
-        // foreach (GameObject inst in objInsts) {
-        //     Vector3 pos = inst.transform.position;
-        //     inst.transform.position = new Vector3(pos.x, pos.y, -1);
-        //     pos = inst.transform.position;
-        //     Debug.Log(inst + " position is " + pos.x + " " + pos.y + " " + pos.z);
-        // }
     }
 }
