aee6685ff3fb1da28c5756e853b519a2573082e3
John Green <38727877+Jaggaroo@users.noreply.github.com>
2020-02-02 10:15:49 -0800

    Name change and video

---
+++ Assets/videoIntroScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class videoIntroScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject camera = GameObject.Find("Main Camera");
+
+        var videoPlayer = camera.AddComponent<UnityEngine.Video.VideoPlayer>();
+
+        videoPlayer.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
