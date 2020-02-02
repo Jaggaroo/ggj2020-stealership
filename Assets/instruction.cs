95377d8f5b200b018e06287e7cfeb5f681365193
John Green <38727877+Jaggaroo@users.noreply.github.com>
2020-02-02 09:24:29 -0800

    freeze rotation of broken ships

---
+++ Assets/instruction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction : MonoBehaviour
+{
+    public GameObject instruction;
+    //public GameObject catInstruction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
